d1ae1f88268e94491fb55bcf47f7e1b1775e0478
sarahhawelu19 <sarahhawelu19@gmail.com>
2025-06-08 16:02:42 -0700

    Updated ViewMeetingDetailsModal.tsx

--- src/components/scheduling/ViewMeetingDetailsModal.tsx
+++ src/components/scheduling/ViewMeetingDetailsModal.tsx
@@ -323,7 +323,7 @@ const ViewMeetingDetailsModal: React.FC<ViewMeetingDetailsModalProps> = ({
                     // Organizer Actions
                     <div>
                       <h4 className="font-medium mb-4 flex items-center gap-2">
-                        <Crown className="text-gold" size={16} />
+                        <Crown className="text-gold\" size={16} />
                         Organizer Actions
                       </h4>
                       
